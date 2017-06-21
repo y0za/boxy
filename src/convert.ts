2cf8bf13b8a5f93b19b1e981c58f4e7d387cf0f8
y0za <yoza.is12s@gmail.com>
2017-06-21 23:52:28 +0900

    Add convert

--- src/convert.ts
+++ src/convert.ts
@@ -1,3 +1,25 @@
+export function convert(value: any, name: String = ''): String {
+  if (Array.isArray(value)) {
+    return convertArray(value as Array<any>, name);
+  } else if (typeof value === 'object' && value !== null) {
+    return convertObject(value, name);
+  }
+
+  if (name === '') {
+    return String(value);
+  } else {
+    return name + ': ' + String(value);
+  }
+}
+
+export function convertObject(obj: Object, name: String = ''): String {
+  return ''
+}
+
+export function convertArray(array: Array<any>, name: String = ''): String {
+  return ''
+}
+
 export function insertArrayPrefix(text: String): String {
   const lines = text.split(/\r\n|\r|\n/)
   return lines.map((line, index) => {
--- src/test/convert.ts
+++ src/test/convert.ts
@@ -1,9 +1,44 @@
 import test from 'ava';
 
 import {
+  convert,
   insertArrayPrefix,
   insertLastArrayPrefix,
 } from '../convert';
+
+test('convert string value', (t) => {
+  const value = 'bar'
+  const name = 'foo'
+  const expected = 'foo: bar'
+  t.is(convert(value, name), expected)
+});
+
+test('convert string with empty name', (t) => {
+  const value = 'bar'
+  const expected = 'bar'
+  t.is(convert(value), expected)
+});
+
+test('convert number value', (t) => {
+  const value = 123
+  const name = 'foo'
+  const expected = 'foo: 123'
+  t.is(convert(value, name), expected)
+});
+
+test('convert null value', (t) => {
+  const value = null
+  const name = 'foo'
+  const expected = 'foo: null'
+  t.is(convert(value, name), expected)
+});
+
+test('convert boolean value', (t) => {
+  const value = true
+  const name = 'foo'
+  const expected = 'foo: true'
+  t.is(convert(value, name), expected)
+});
 
 test('insertArrayPrefix single line', (t) => {
   const text = 'foo';
