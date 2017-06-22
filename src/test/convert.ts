08c83246f78d169f197bfa3a6c12eddd6ae9b81c
y0za <yoza.is12s@gmail.com>
2017-06-22 18:59:23 +0900

    Add missing semicolons in src/test/convert.ts

--- src/test/convert.ts
+++ src/test/convert.ts
@@ -7,37 +7,37 @@ import {
 } from '../convert';
 
 test('convert string value', (t) => {
-  const value = 'bar'
-  const name = 'foo'
-  const expected = 'foo: bar'
-  t.is(convert(value, name), expected)
+  const value = 'bar';
+  const name = 'foo';
+  const expected = 'foo: bar';
+  t.is(convert(value, name), expected);
 });
 
 test('convert string with empty name', (t) => {
-  const value = 'bar'
-  const expected = 'bar'
-  t.is(convert(value), expected)
+  const value = 'bar';
+  const expected = 'bar';
+  t.is(convert(value), expected);
 });
 
 test('convert number value', (t) => {
-  const value = 123
-  const name = 'foo'
-  const expected = 'foo: 123'
-  t.is(convert(value, name), expected)
+  const value = 123;
+  const name = 'foo';
+  const expected = 'foo: 123';
+  t.is(convert(value, name), expected);
 });
 
 test('convert null value', (t) => {
-  const value = null
-  const name = 'foo'
-  const expected = 'foo: null'
-  t.is(convert(value, name), expected)
+  const value = null;
+  const name = 'foo';
+  const expected = 'foo: null';
+  t.is(convert(value, name), expected);
 });
 
 test('convert boolean value', (t) => {
-  const value = true
-  const name = 'foo'
-  const expected = 'foo: true'
-  t.is(convert(value, name), expected)
+  const value = true;
+  const name = 'foo';
+  const expected = 'foo: true';
+  t.is(convert(value, name), expected);
 });
 
 test('insertArrayPrefix single line', (t) => {
